7f05c33af2358fdb5f9e9ec291c93ab5ffa756d2
QUSIZHAO <1424757216@qq.com>
2018-06-15 10:39:45 +0800

    界面侧滑

---
+++ app/src/main/java/com/qsz/materialdesign/Fruit.java
@@ -0,0 +1,27 @@
+package com.qsz.materialdesign;
+
+/**
+ * Created by QSZ on 2018/6/14 14:16
+ *
+ * @author Administrator
+ */
+public class Fruit {
+
+    private String name;
+
+    private int imageId;
+
+    public Fruit(String name, int imageId) {
+        this.name = name;
+        this.imageId = imageId;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public int getImageId() {
+        return imageId;
+    }
+
+}
